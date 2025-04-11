18764142687c2984b1d190b7b9c33323a1e98f10
Lz <imlangzi@qq.com>
2025-04-11 23:28:33 +0800

    feat(xray): use `DialXX` instead of `PorxyXX` in xray (#12)

--- xray/proxy_ssr.go
+++ xray/proxy_ssr.go
@@ -1,7 +1,9 @@
 package xray
 
 import (
+	"context"
 	"fmt"
+	"net"
 	"net/http"
 	"net/url"
 
@@ -9,7 +11,7 @@ import (
 )
 
 func init() {
-	proxyclient.RegisterProxy("ssr", ProxySSR)
+	proxyclient.RegisterProxy("ssr", DialSSR)
 }
 
 // ProxySSR creates a RoundTripper for SSR proxy
@@ -17,10 +19,28 @@ func ProxySSR(u *url.URL, o *proxyclient.Options) (http.RoundTripper, error) {
 	// Start SSR client through Xray
 	_, port, err := StartSSR(u, 0)
 	if err != nil {
-		return nil, fmt.Errorf("failed to start SSR proxy: %w", err)
+		return nil, fmt.Errorf("failed to start ssr proxy: %w", err)
 	}
 
 	// Use SOCKS5 proxy created by Xray
 	proxyURL, _ := url.Parse(fmt.Sprintf("socks5://127.0.0.1:%d", port))
 	return proxyclient.ProxySocks5(proxyURL, o)
+}
+
+// DialSSR creates a custom transport that dials directly to the v2ray server
+// instead of using a local SOCKS proxy.
+func DialSSR(u *url.URL, o *proxyclient.Options) (http.RoundTripper, error) {
+	instance, _, err := StartSSR(u, 0)
+	if err != nil {
+		return nil, fmt.Errorf("failed to start ssr proxy: %w", err)
+	}
+
+	// Create a transport that uses our custom dialer
+	tr := proxyclient.CreateTransport(o)
+	tr.DialContext = func(ctx context.Context, network, addr string) (net.Conn, error) {
+		return dialContext(ctx, instance, network, addr)
+	}
+	tr.Proxy = nil
+
+	return tr, nil
 }
--- xray/proxy_trojan.go
+++ xray/proxy_trojan.go
@@ -1,7 +1,9 @@
 package xray
 
 import (
+	"context"
 	"fmt"
+	"net"
 	"net/http"
 	"net/url"
 
@@ -9,7 +11,7 @@ import (
 )
 
 func init() {
-	proxyclient.RegisterProxy("trojan", ProxyTrojan)
+	proxyclient.RegisterProxy("trojan", DialTrojan)
 }
 
 // ProxyTrojan creates a RoundTripper for Trojan proxy
@@ -23,4 +25,22 @@ func ProxyTrojan(u *url.URL, o *proxyclient.Options) (http.RoundTripper, error) 
 	// Use SOCKS5 proxy created by Xray
 	proxyURL, _ := url.Parse(fmt.Sprintf("socks5://127.0.0.1:%d", port))
 	return proxyclient.ProxySocks5(proxyURL, o)
+}
+
+// DialTrojan creates a custom transport that dials directly to the v2ray server
+// instead of using a local SOCKS proxy.
+func DialTrojan(u *url.URL, o *proxyclient.Options) (http.RoundTripper, error) {
+	instance, _, err := StartTrojan(u, 0)
+	if err != nil {
+		return nil, fmt.Errorf("failed to start trojan proxy: %w", err)
+	}
+
+	// Create a transport that uses our custom dialer
+	tr := proxyclient.CreateTransport(o)
+	tr.DialContext = func(ctx context.Context, network, addr string) (net.Conn, error) {
+		return dialContext(ctx, instance, network, addr)
+	}
+	tr.Proxy = nil
+
+	return tr, nil
 }
