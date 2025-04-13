48954bbc1491fe04f83a2db7d68bcfdc9b5294be
Lz <imlangzi@qq.com>
2025-04-13 22:42:12 +0800

    fix(url): added IsHost/IsIP/IsDomain

--- url.go
+++ url.go
@@ -3,6 +3,8 @@ package proxyclient
 import (
 	"net"
 	"net/url"
+	"regexp"
+	"strings"
 )
 
 type FuncParser func(u *url.URL) (URL, error)
@@ -36,11 +38,7 @@ func (u *stdURL) Opaque() string {
 }
 
 func (u *stdURL) Host() string {
-	h, _, err := net.SplitHostPort(u.URL.Host)
-	if err != nil {
-		return u.URL.Host
-	}
-	return h
+	return u.URL.Hostname()
 }
 func (u *stdURL) Port() string {
 	return u.URL.Port()
@@ -74,5 +72,32 @@ func ParseURL(u string) (URL, error) {
 		return parser(parsedURL)
 	}
 
-	return &stdURL{*parsedURL}, nil
+	if IsHost(parsedURL.Hostname()) {
+		return &stdURL{*parsedURL}, nil
+	}
+
+	return nil, ErrInvalidHost
+}
+
+func IsHost(s string) bool {
+	return IsIP(s) && IsDomain(s)
+}
+
+func IsIP(s string) bool {
+	return net.ParseIP(s) != nil
+}
+
+var regexDomain = regexp.MustCompile(`^([a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}$`)
+
+func IsDomain(s string) bool {
+	if !regexDomain.MatchString(strings.ToLower(s)) {
+		return false
+	}
+
+	ips, err := net.LookupIP(s)
+	if err != nil {
+		return false
+	}
+
+	return len(ips) > 0
 }
