b16119cb64aaa02084ef7a23cb391beca7871a44
Lz <imlangzi@qq.com>
2025-04-29 09:08:30 +0800

    fix(socks): used h12.io/socks instead of x/net/proxy (#20)

--- proxy_socks.go
+++ proxy_socks.go
@@ -12,7 +12,7 @@ import (
 )
 
 func init() {
-	supportProxies["socks5"] = ProxySocks5
+	supportProxies["socks5"] = ProxySocks4
 	supportProxies["socks5h"] = ProxySocks5
 	supportProxies["socks4"] = ProxySocks4
 	supportProxies["socks4a"] = ProxySocks4
